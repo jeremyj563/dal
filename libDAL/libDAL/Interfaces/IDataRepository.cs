7488e010af1fc8821828708ceda25e4da7ed33d3
jeremyj563 <modernclassicgamer@gmail.com>
2019-06-28 14:31:17 -0500

    Update IDataRepository.cs

--- libDAL/libDAL/Interfaces/IDataRepository.cs
+++ libDAL/libDAL/Interfaces/IDataRepository.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace DataRepositories.Interfaces
 {
@@ -12,7 +13,7 @@ namespace DataRepositories.Interfaces
         /// <param name="cmd">The command to create the new record.</param>
         /// <param name="record">The domain object instance containing the new record.</param>
         /// <returns>The ID of the newly created record.</returns>
-        int New<T>(string cmd, T record) where T : new();
+        Task<int> NewAsync<T>(string cmd, T record) where T : new();
 
         /// <summary>
         /// Creates new records according to the schema defined by the specified type.
@@ -20,7 +21,7 @@ namespace DataRepositories.Interfaces
         /// <typeparam name="T">The type that defines the record schema.</typeparam>
         /// <param name="cmd">The command to create the new records.</param>
         /// <param name="records">The collection of domain object instances containing the new records.</param>
-        void New<T>(IEnumerable<T> records, string tableName) where T : new();
+        Task NewAsync<T>(IEnumerable<T> records, string tableName) where T : new();
 
         /// <summary>
         /// Retrieves a collection of records according to the schema defined by the specified type.
@@ -29,7 +30,7 @@ namespace DataRepositories.Interfaces
         /// <param name="cmd">The command to retrieve the records.</param>
         /// <param name="params">The optional parameters to be added to the command.</param>
         /// <returns>A collection of domain object instances representing the records returned from the command.</returns>
-        IQueryable<T> Get<T>(string cmd, (string, object)[] @params = null) where T : new();
+        Task<IQueryable<T>> GetAsync<T>(string cmd, (string, object)[] @params = null) where T : new();
 
         /// <summary>
         /// Edits a record according to the schema defined by the specified type.
@@ -39,7 +40,7 @@ namespace DataRepositories.Interfaces
         /// <param name="record">The domain object instance containing the edited record.</param>
         /// <param name="params">The optional parameters to be added to the command.</param>
         /// <returns>The ID of the edited record.</returns>
-        int Edit<T>(string cmd, T record, (string, object)[] @params = null) where T : new();
+        Task<int> EditAsync<T>(string cmd, T record, (string, object)[] @params = null) where T : new();
 
         /// <summary>
         /// Removes a record according to the schema defined by the specified type.
@@ -49,6 +50,12 @@ namespace DataRepositories.Interfaces
         /// <param name="record">The domain object instance containing the record to remove.</param>
         /// <param name="params">The optional parameters to be added to the command.</param>
         /// <returns>The ID of the removed record.</returns>
-        int Remove<T>(string cmd, T record, (string, object)[] @params = null) where T : new();
+        Task<int> RemoveAsync<T>(string cmd, T record, (string, object)[] @params = null) where T : new();
+
+        /// <summary>
+        /// Tests the connection availability by attempting to open the connection.
+        /// </summary>
+        /// <returns>True if the connection attempt succeeds or false if it fails.</returns>
+        Task<bool> IsConnectionAvailableAsync();
     }
 }
