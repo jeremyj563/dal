49b69542fc0c613042d29dbb8e9ee0994594ca95
jeremyj563 <modernclassicgamer@gmail.com>
2019-06-28 14:32:39 -0500

    Update Program.cs

--- libDAL/TestConsoleApp/Program.cs
+++ libDAL/TestConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using Models;
 using DataRepositories;
 
@@ -6,7 +6,7 @@ namespace TestConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
             // Instantiate a new SQL Server connection
             var conn = "Data Source=SQL01;Initial Catalog=db;Persist Security Info=True;User ID=user;Password=pass";
@@ -14,24 +14,27 @@ namespace TestConsoleApp
 
             // Create a new 'Employee' with auto generated id
             var newEmp = new Employee() { Name = "Jeremy Johnson", Email = "jmjohnson@ci.davenport.ia.us" };
-            newEmp.ID = db.New("INSERT INTO [Employees] ([Name], [Email]) VALUES (@Name, @Email)", record: newEmp);
+            var newTask = db.NewAsync("INSERT INTO [Employees] ([Name], [Email]) VALUES (@Name, @Email)", record: newEmp);
+            newEmp.ID = newTask.Result;
 
             // Get all 'Employees'
-            var emps = db.Get<Employee>("SELECT [ID],[Name],[Email] FROM [Employees]");
+            var getTask = db.GetAsync<Employee>("SELECT [ID],[Name],[Email] FROM [Employees]");
+            var emps = getTask.Result;
 
             // Bulk insert all 'Employees' into the 'PR_Employees' table
-            db.New(emps, "PR_Employees");
+            await db.NewAsync(emps, "PR_Employees");
 
             // Get 'Employee' with id 123
             (string, object)empID = (nameof(Employee.ID), 123);
-            var emp123 = db.Get<Employee>("SELECT [ID],[Name],[Email] FROM [Employees] WHERE [ID] = @ID", @params: new[] {empID}).First();
+            getTask = db.GetAsync<Employee>("SELECT [ID],[Name],[Email] FROM [Employees] WHERE [ID] = @ID", @params: new[] { empID });
+            var emp123 = getTask.Result.First();
 
             // Update email address for 'Employee' 123
             emp123.Email = "new@email.com";
-            db.Edit("UPDATE [Employees] SET [Email] = '@Email' WHERE [ID] = @ID", record: emp123);
+            await db.EditAsync("UPDATE [Employees] SET [Email] = '@Email' WHERE [ID] = @ID", record: emp123);
 
             // Delete 'Employee' 123
-            db.Remove("DELETE FROM [Employees] WHERE [ID] = @ID", record: emp123);
+            await db.RemoveAsync("DELETE FROM [Employees] WHERE [ID] = @ID", record: emp123);
         }
     }
 }
