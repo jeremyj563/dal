4bbbec97538614b797c636e0c098d6ad17ef7ad9
jeremyj563 <modernclassicgamer@gmail.com>
2019-09-24 06:05:44 -0500

    Update MYSQLRepository.cs

--- libDAL/libDAL/Repositories/MYSQLRepository.cs
+++ libDAL/libDAL/Repositories/MYSQLRepository.cs
@@ -9,56 +9,58 @@ namespace DataRepositories
 {
     public class MYSQLRepository : BaseSQLRepository
     {
-        public MYSQLRepository(string connectionString, string idCommand = "; SELECT LAST_INSERT_ID();") : base(connectionString, idCommand)
+        public MYSQLRepository(string connectionString, string idCommand = "; SELECT LAST_INSERT_ID();") 
+            : base(connectionString, idCommand)
         {
         }
 
-        public async override Task<int> NewAsync<TSchema>(string cmd, TSchema schema)
+        #region Public Methods
+
+        public override async Task<int> NewAsync<TSchema>(string cmd, TSchema schema)
         {
             var id = await base.NonQueryAsync<MySqlConnection, MySqlCommand, TSchema>(cmd, schema, null);
             return id;
         }
 
-        public async override Task NewAsync<TSchema>(IEnumerable<TSchema> instances, string tableName)
+        public override async Task NewAsync<TSchema>(IEnumerable<TSchema> instances, string tableName)
         {
             throw new NotImplementedException();
         }
 
-        public async override Task<IQueryable<TSchema>> GetAsync<TSchema>(string cmd, (string, object)[] @params = null)
+        public override async Task<IQueryable<TSchema>> GetAsync<TSchema>(string cmd, (string, object)[] @params = null)
         {
             var instances = await base.QueryAsync<MySqlConnection, MySqlCommand, TSchema>(cmd, @params);
             return instances;
         }
 
-        public async override Task<IQueryable<Dynamic>> GetDynamicAsync(Dynamic schema, string cmd, (string, object)[] @params = null)
+        public override async Task<IQueryable<Dynamic>> GetDynamicAsync(Dynamic schema, string cmd, (string, object)[] @params = null)
         {
             var instances = await base.QueryDynamicAsync<MySqlConnection, MySqlCommand>(schema, cmd, @params);
             return instances;
         }
 
-        public async override Task<int> EditAsync<TSchema>(string cmd, TSchema schema = default, (string, object)[] @params = null)
+        public override async Task<int> EditAsync<TSchema>(string cmd, TSchema schema = default, (string, object)[] @params = null)
         {
             var id = await base.NonQueryAsync<MySqlConnection, MySqlCommand, TSchema>(cmd, schema, @params);
             return id;
         }
 
-        public async override Task<int> RemoveAsync<TSchema>(string cmd, TSchema schema = default, (string, object)[] @params = null)
+        public override async Task<int> RemoveAsync<TSchema>(string cmd, TSchema schema = default, (string, object)[] @params = null)
         {
             var id = await base.NonQueryAsync<MySqlConnection, MySqlCommand, TSchema>(cmd, schema, @params);
             return id;
         }
 
-        public async override Task<bool> IsConnectionAvailableAsync()
+        public override async Task<bool> IsConnectionAvailableAsync()
         {
-            bool result = default(bool);
-            try
-            {
-                result = await base.OpenConnectionAsync<MySqlConnection>();
-                base.Connection.Close();
-            }
+            bool result = false;
+            try { result = await base.OpenConnectionAsync<MySqlConnection>(); }
             catch (Exception) { throw; }
+            finally { base.Connection.Close(); }
 
             return result;
         }
+
+        #endregion
     }
 }
